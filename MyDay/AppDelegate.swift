d4345a668715395d27fde39af292564818f29653
Danny Huang <huangdanny21@gmail.com>
2021-08-15 09:44:26 -0700

    tmep

--- MyDay/AppDelegate.swift
+++ MyDay/AppDelegate.swift
@@ -11,11 +11,17 @@ import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    var appCoordinator: AppCoordinator!
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         MDUser.shared
-        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        appCoordinator = AppCoordinator(window: window)
+        appCoordinator.start()
         return true
     }
     
